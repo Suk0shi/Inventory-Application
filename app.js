fdb68a880d19fb6439ca7bd5330ae800aae92100
Sukoshi <hugo.cordon100@gmail.com>
2024-02-27 11:26:41 +0000

    Make server adjustments

--- app.js
+++ app.js
@@ -7,14 +7,38 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const inventoryRouter = require('./routes/inventory');
+const compression = require("compression")
+const helmet = require("helmet");
 
 const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 
 // Set up mongoose connection 
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const mongoDB = 'mongodb+srv://hugocordon100:CaTf9PwVZ89QezkF@cluster0.7gtdzfw.mongodb.net/inventory?retryWrites=true&w=majority&appName=Cluster0';
+const dev_db_url =
+'';
+
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 main().catch((err) => console.log(err));
 async function main() {
